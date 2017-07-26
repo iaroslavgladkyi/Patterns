4f48944ccdf572e799ad3d2f25dcb363e33ab43c
iaroslavgladkyi <igladkyi@gmail.com>
2017-07-26 20:25:07 +0300

    add example of Singleton pattern

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,21 @@
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+
+}
---
+++ Singleton/src/Start.java
@@ -0,0 +1,8 @@
+public class Start {
+
+    public static void main(String[] args) {
+        Singleton.getInstance().print();
+        Singleton.getInstance().print();
+        Singleton.getInstance().print();
+    }
+}
